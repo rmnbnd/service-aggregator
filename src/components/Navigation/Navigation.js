b5022ed0ddb6d438f579ea4993df462c2e8a7ae7
Roman Bondarchuk <rmnbnd@gmail.com>
2016-07-03 13:49:51 +0300

    [F] - Remove login and register pages

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,22 +1,13 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import React, { Component, PropTypes } from 'react';
-import cx from 'classnames';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Navigation.scss';
-import Link from '../Link';
+import React, {Component, PropTypes} from "react";
+import cx from "classnames";
+import withStyles from "isomorphic-style-loader/lib/withStyles";
+import s from "./Navigation.scss";
+import Link from "../Link";
 
 class Navigation extends Component {
 
   static propTypes = {
-    className: PropTypes.string,
+    className: PropTypes.string
   };
 
   render() {
@@ -24,10 +15,6 @@ class Navigation extends Component {
       <div className={cx(s.root, this.props.className)} role="navigation">
         <Link className={s.link} to="/about">About</Link>
         <Link className={s.link} to="/contact">Contact</Link>
-        <span className={s.spacer}> | </span>
-        <Link className={s.link} to="/login">Log in</Link>
-        <span className={s.spacer}>or</span>
-        <Link className={cx(s.link, s.highlight)} to="/register">Sign up</Link>
       </div>
     );
   }
